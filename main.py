8d928a270e8308a45c1f5f0aa11917e15e20c03f
StijnDeVos <stijndev@gmail.com>
2021-10-24 14:45:55 +0200

    Stub

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collatz import collatz
+from collatz import collatz, plot_sequence
 from colorama import Fore, init
 
 init(autoreset = True)
@@ -7,17 +7,25 @@ even = lambda n: f"{Fore.GREEN}{n}"
 odd = lambda n: f"{Fore.RED}{n}"
 check_color = lambda n: even(n) if n % 2 == 0 else odd(n)
 
+def parse_input(n):
+	try:
+		n = int(n)
+	except Exception as e:
+		if n == "plot":
+			print("Plot functionality coming soon.")
+		print(f"Error: {e}")
+	else:
+		if n <= 0:
+			print("Collatz(n) not defined for n <= 0.")
+		else:
+			c = collatz(n).nr_iters_to_one()
+			print(" -> ".join([check_color(n) for n in c]))
+
 def collatz_repl():
 	try:
 		while True:
-			n = input(">> ")
-			try:
-				n = int(n)				
-			except Exception as e:
-				print(f"Error: {e}")
-			else:
-				c = collatz(n).nr_iters_to_one()
-				print(" -> ".join([check_color(n) for n in c]))
+			n = input("collatz_repl > ")
+			parse_input(n)
 	except KeyboardInterrupt as e:
 		print("\nExiting...")
 
